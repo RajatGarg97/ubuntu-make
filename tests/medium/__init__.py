88667afb0064e1ab4ad544ed74e2d174a75b722e
Galileo Sartor <galileo.sartor@gmail.com>
2018-10-11 13:29:41 +0200

    ide: Add liteide support

--- tests/medium/__init__.py
+++ tests/medium/__init__.py
@@ -31,7 +31,7 @@ class ContainerTests(LoggedTestCase):
     """Container-based tests utilities"""
 
     DOCKER_USER = "user"
-    DOCKER_TESTIMAGE = "lyzardking/ubuntu-make"
+    DOCKER_TESTIMAGE = "2f8395672d9c"
     UMAKE_TOOLS_IN_CONTAINER = "/umake"
     APT_FAKE_REPO_PATH = "/apt-fake-repo"
 
